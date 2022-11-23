34fc31248fe795d403037386330bd20063f71a5b
Julien Loctaux <96430950+azert9@users.noreply.github.com>
2022-11-23 23:33:49 +0100

    Fix PATH caching issue with bash

--- internal/env/activate.in.sh
+++ internal/env/activate.in.sh
@@ -4,40 +4,46 @@
 # unset variables and commands returning non-0 status.
 
 if [ -n "${GOENV_PATH:-}" ]; then
-  echo "An environment is already active (deactivate with \"deactivate\")." >&2
+	echo "An environment is already active (deactivate with \"deactivate\")." >&2
 else
-  deactivate() {
-    PATH="$GOENV_SAVED_PATH"
+	deactivate() {
+		PATH="$GOENV_SAVED_PATH"
 
-    GOROOT="$GOENV_SAVED_GOROOT"
-    if [ -z "$GOROOT" ]; then
-      unset GOROOT
-    fi
+		GOROOT="$GOENV_SAVED_GOROOT"
+		if [ -z "$GOROOT" ]; then
+			unset GOROOT
+		fi
 
-    GOPATH="$GOENV_SAVED_GOPATH"
-    if [ -z "$GOPATH" ]; then
-      unset GOPATH
-    fi
+		GOPATH="$GOENV_SAVED_GOPATH"
+		if [ -z "$GOPATH" ]; then
+			unset GOPATH
+		fi
 
-    unset GOENV_PATH
-    unset GOENV_SAVED_PATH
-    unset GOENV_SAVED_GOROOT
-    unset GOENV_SAVED_GOPATH
-    unset -f deactivate
-  }
+		unset GOENV_PATH
+		unset GOENV_SAVED_PATH
+		unset GOENV_SAVED_GOROOT
+		unset GOENV_SAVED_GOPATH
+		unset -f deactivate
+	}
 
-  export GOENV_PATH={{ .Dir.Escape }}
+	export GOENV_PATH={{ .Dir.Escape }}
 
-  GOENV_SAVED_GOROOT="${GOROOT:-}"
-  export GOROOT={{ .GoRoot.Escape }}
-  echo GOROOT={{ .GoRoot.Escape.Escape }}
+	GOENV_SAVED_GOROOT="${GOROOT:-}"
+	export GOROOT={{ .GoRoot.Escape }}
+	echo GOROOT={{ .GoRoot.Escape.Escape }}
 
-  GOENV_SAVED_GOPATH="${GOPATH:-}"
-  export GOPATH={{ .GoPath.Escape }}
-  echo GOPATH={{ .GoPath.Escape.Escape }}
+	GOENV_SAVED_GOPATH="${GOPATH:-}"
+	export GOPATH={{ .GoPath.Escape }}
+	echo GOPATH={{ .GoPath.Escape.Escape }}
 
-  GOENV_SAVED_PATH="$PATH"
-  PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+	GOENV_SAVED_PATH="$PATH"
+	PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
 
-  echo "Deactivate with: deactivate"
+	# Clearing PATH search cache.
+	# TODO: handle more shells
+	if [ -n "${BASH_VERSION:-}" ]; then
+		hash -d go 2> /dev/null || true
+	fi
+
+	echo "Deactivate with: deactivate"
 fi
